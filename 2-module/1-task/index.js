48b45d72e4cb0634dcc07f95875703c90283762c
Anton Kharlamov <antonio100500@ya.ru>
2020-10-02 14:59:33 +0300

    решение первой задачи третьего модуля

--- 2-module/1-task/index.js
+++ 2-module/1-task/index.js
@@ -5,5 +5,12 @@
  */
 
 function sumSalary(salaries) {
-    
+    let sumOfSalaries = 0;
+  for (let key in salaries) {
+    let isSalary = typeof salaries[key] === 'number';
+    if (isSalary) {
+      sumOfSalaries += salaries[key];
+    }
+  }
+  return sumOfSalaries;
    }
